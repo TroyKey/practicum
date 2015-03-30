a5a0bf7af926c5ca3d0755cdbc30ba9ba22c43c2
TroyKey <tkv9795@saintjoe.edu>
2015-03-30 12:28:09 -0500

    Update date.js

--- date.js
+++ date.js
@@ -4,5 +4,6 @@ function findDate(string) {
   return new Date(Number(match[3]),
                   Number(match[1]) - 1,
                   Number(match[2]));
+                  //tested  with switching around the 1,2,3 and i got it to display correctly.
 }
 print(findDate("30-1-2003"));
