b36dbca50ab84b426a294fa803f5db8af680df6a
TroyKey <tkv9795@saintjoe.edu>
2015-03-30 12:30:33 -0500

    Update match.js

--- match.js
+++ match.js
@@ -1,5 +1,6 @@
 var input = "A string with 7 numbers in it... 42 and 88, 123 and 16, 4 and 22.";
 var number = /\b(\d[0-9])\b/g;
+//tested different combos of the regExp \b [0-9] and endd up replacing the + to get it to work
 var match;
 while (match = number.exec(input))
   console.log("Found", match[1], "at", match.index);
