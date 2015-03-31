aed444605f82fd2344092e1c62583df55d7a0c21
TroyKey <tkv9795@saintjoe.edu>
2015-03-30 20:11:11 -0500

    Update counter.js

--- counter.js
+++ counter.js
@@ -5,6 +5,11 @@ function minusOne(match, amount, unit) {
     unit = unit.slice(0, unit.length - 1);
   else if (amount == 0)
     amount = "no";
+  else if (amount > 99)//wasnt sure if you wanted "zillions of" if it had 100 eggs
+  //so this will make it say "zillions of" if it has 100 eggs if you wanted it tochange at 101 just
+  //change he 99 to 100
+    amount = "zillions of";
+    //if the amount is > then 100 if till say "zillions of" instead of the number 
   return amount + " " + unit;
 }
 print(stock.replace(/(\d+) (\w+)/g, minusOne));
