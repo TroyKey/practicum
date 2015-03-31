963b79be757c8f3dae5a97d61060bf79d9f0585b
TroyKey <tkv9795@saintjoe.edu>
2015-03-30 20:26:12 -0500

    Update counter.js

--- counter.js
+++ counter.js
@@ -5,6 +5,9 @@ function minusOne(match, amount, unit) {
     unit = unit.slice(0, unit.length - 1);
   else if (amount == 0)
     amount = "no";
+  else if (amount > 99)// if the amount is > 99 it will display "zillions of" instead of the number
+  //i wasnt sure if you want it to say "zillions of" if it was at 100 or 101  if that was the case change the 99 to 100
+    amount ="zillions of";
   return amount + " " + unit;
 }
 print(stock.replace(/(\d+) (\w+)/g, minusOne));
